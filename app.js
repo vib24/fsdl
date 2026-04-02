b211b80ec75fdc1acccd107493bd9d600294633c
vib24 <vibhanshimoon04@gmail.com>
2026-04-02 11:21:38 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Form from "./components/Form";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Resume Builder</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
